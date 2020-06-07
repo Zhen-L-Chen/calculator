047bfeeeea3a8a66956ba330fd703210c6cc5bbf
Zhen-L-Chen <zhen.l.chen@mail.mcgill.ca>
2020-06-07 11:06:02 -0400

    multiple chained operations capability added

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ let firstNum = "";
 let secondNum = "";
 let temporaryNum = "";
 let operator = "";
-
-
+let tempOp = "";
 
 function add(a, b){
     return a + b;
@@ -19,7 +18,11 @@ function multiply(a, b){
 };
 
 function divide(a, b){
-    return a / b;
+    if(b !== 0){
+        return a / b;  
+    }else{
+        return 'cannot divide by 0';
+    }
 }
 
 function operate(operator, a, b){
@@ -37,7 +40,7 @@ function operate(operator, a, b){
 }
 
 /* DOM calculator buttons */
-const calBody = document.querySelector('.answers');
+const calBody = document.querySelector('.buttons');
 function initButtons(num){
     for(let i = 0; i < num; i++){
         const newDiv = document.createElement("button");
@@ -46,21 +49,40 @@ function initButtons(num){
         calBody.appendChild(newDiv);
     }
 }
-
+/* intialize the buttons with the numbers*/
 initButtons(10);
 
+/* update input live*/
+let buttons = document.querySelector('.buttons');
 let answers = document.querySelector('.answers');
 
 function single(e){
     temporaryNum += e.target.innerText;
-    console.log(temporaryNum);
+    answers.textContent = temporaryNum;
 }; 
 
-answers.addEventListener('click', single);
+buttons.addEventListener('click', single);
 
+function updateTempNum(){
+    if(firstNum === ""){
+        firstNum = temporaryNum;
+    }else{
+        a = Number(firstNum);
+        b = Number(temporaryNum);
+        firstNum = operate(tempOp, a, b);
+    }
+}
+
+/* temporary number, first number updated when an operator is clicked on*/
 function updateOp(e){
-    operator = e.target.innerText;
-    firstNum = temporaryNum;
+    if(operator === ""){
+        operator = e.target.innerText;
+        updateTempNum();
+    }else{
+        tempOp = operator;
+        operator = e.target.innerText
+        updateTempNum();
+    }
     temporaryNum = "";
 };
 
@@ -68,11 +90,13 @@ let operating = document.querySelector('.operator');
 
 operating.addEventListener('click', updateOp);
 
+/*compute results when the equal sign is clicked on*/
 function equal(e){
     secondNum = temporaryNum;
     let a = Number(firstNum);
     let b = Number(secondNum);
     let result = operate(operator, a, b);
+    answers.textContent = result;
     firstNum = "";
     secondNum = "";
     temporaryNum = "";
